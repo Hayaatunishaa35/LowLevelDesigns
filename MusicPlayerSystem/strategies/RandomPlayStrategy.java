ebf7186dc5fa6c47fbf5446c7034d1883cbec52f
Hayaatunishaa35 <hayatunisha1@gmail.com>
2026-02-14 20:31:47 +0530

    PlayStrategy fixes

--- MusicPlayerSystem/strategies/RandomPlayStrategy.java
+++ MusicPlayerSystem/strategies/RandomPlayStrategy.java
@@ -1,26 +1,44 @@
 package strategies;
 
+import java.util.*;
+
 import models.Playlist;
 import models.Song;
 
 public class RandomPlayStrategy implements PlayStrategy{
+    private List<Song> shuffledPlaylist;
+    private int currentIndex;
 
     @Override
     public void setPlaylist(Playlist playlist) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setPlaylist'");
+        if(playlist==null||playlist.getSongs().size()==0){
+            throw new RuntimeException("Playlist is empty.");
+        }
+        shuffledPlaylist = new ArrayList<>(playlist.getSongs());
+        Collections.shuffle(shuffledPlaylist);
+        currentIndex = -1;
     }
 
     @Override
     public Song playNext() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'playNext'");
+        currentIndex++;
+        if(currentIndex==shuffledPlaylist.size()){
+            Collections.shuffle(shuffledPlaylist);
+            currentIndex = 0;
+        }
+
+        return shuffledPlaylist.get(currentIndex);
     }
 
     @Override
     public Song playPrevious() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'playPrevious'");
+        currentIndex--;
+        if(currentIndex==0){
+            Collections.shuffle(shuffledPlaylist);
+            currentIndex = shuffledPlaylist.size()-1;
+        }
+
+        return shuffledPlaylist.get(currentIndex);
     }
 
     
